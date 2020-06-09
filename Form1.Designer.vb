5bf7412f4c3d4806e6dd10b87138c70fd16596dc
alexandra madeira <63977887+asmthepenguin@users.noreply.github.com>
2020-06-09 11:39:13 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnReload = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.LblID = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.txtUtilizador = New System.Windows.Forms.TextBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.DGLista = New System.Windows.Forms.DataGridView()
+        Me.CbNivel = New System.Windows.Forms.ComboBox()
+        CType(Me.DGLista, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCreate.Location = New System.Drawing.Point(508, 29)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(87, 35)
+        Me.btnCreate.TabIndex = 0
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'btnReload
+        '
+        Me.btnReload.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReload.Location = New System.Drawing.Point(508, 83)
+        Me.btnReload.Name = "btnReload"
+        Me.btnReload.Size = New System.Drawing.Size(87, 35)
+        Me.btnReload.TabIndex = 1
+        Me.btnReload.Text = "Reload"
+        Me.btnReload.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(508, 139)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(87, 35)
+        Me.btnUpdate.TabIndex = 2
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(508, 193)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(87, 35)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'LblID
+        '
+        Me.LblID.AutoSize = True
+        Me.LblID.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblID.Location = New System.Drawing.Point(119, 29)
+        Me.LblID.Name = "LblID"
+        Me.LblID.Size = New System.Drawing.Size(23, 18)
+        Me.LblID.TabIndex = 4
+        Me.LblID.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(82, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 18)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Nome:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(59, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 18)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Utilizador:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(79, 142)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Senha:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 184)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(124, 18)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Nivel de Acesso:"
+        '
+        'txtNome
+        '
+        Me.txtNome.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome.Location = New System.Drawing.Point(142, 58)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(299, 26)
+        Me.txtNome.TabIndex = 9
+        '
+        'txtUtilizador
+        '
+        Me.txtUtilizador.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUtilizador.Location = New System.Drawing.Point(142, 97)
+        Me.txtUtilizador.Name = "txtUtilizador"
+        Me.txtUtilizador.Size = New System.Drawing.Size(299, 26)
+        Me.txtUtilizador.TabIndex = 10
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSenha.Location = New System.Drawing.Point(142, 139)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.txtSenha.Size = New System.Drawing.Size(299, 26)
+        Me.txtSenha.TabIndex = 11
+        '
+        'DGLista
+        '
+        Me.DGLista.AllowUserToAddRows = False
+        Me.DGLista.AllowUserToDeleteRows = False
+        Me.DGLista.AllowUserToResizeColumns = False
+        Me.DGLista.AllowUserToResizeRows = False
+        Me.DGLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGLista.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.DGLista.Location = New System.Drawing.Point(23, 250)
+        Me.DGLista.Name = "DGLista"
+        Me.DGLista.RowHeadersVisible = False
+        Me.DGLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DGLista.Size = New System.Drawing.Size(585, 150)
+        Me.DGLista.TabIndex = 12
+        '
+        'CbNivel
+        '
+        Me.CbNivel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CbNivel.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbNivel.FormattingEnabled = True
+        Me.CbNivel.Items.AddRange(New Object() {"Equipa", "Administrador"})
+        Me.CbNivel.Location = New System.Drawing.Point(142, 181)
+        Me.CbNivel.Name = "CbNivel"
+        Me.CbNivel.Size = New System.Drawing.Size(299, 26)
+        Me.CbNivel.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(635, 415)
+        Me.Controls.Add(Me.CbNivel)
+        Me.Controls.Add(Me.DGLista)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtUtilizador)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LblID)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnReload)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Name = "Form1"
+        Me.Text = "CRUD - utilizadores"
+        CType(Me.DGLista, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents btnReload As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents LblID As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtNome As TextBox
+    Friend WithEvents txtUtilizador As TextBox
+    Friend WithEvents txtSenha As TextBox
+    Friend WithEvents DGLista As DataGridView
+    Friend WithEvents CbNivel As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+
+    Private Sub BtnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        Try
+            create("INSERT INTO Utilizadores(nome, utilizador, senha, nivel) VALUES ('" & txtNome.Text & "','" & txtUtilizador.Text & "','" & txtSenha.Text & "','" & CbNivel.SelectedIndex + 1 & ")")
+        Catch ex As Exception
+            'em caso de erro exibir o mesmo 
+            MessageBox.Show("ERRO" & ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BtnReload_Click(sender As Object, e As EventArgs) Handles btnReload.Click
+        Try
+            reload("SELECT * FROM Utilizadores ORDER BY ID ASC", DGLista)
+        Catch ex As Exception
+            MessageBox.Show("ERRO" & ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            updates("UPDATE Utilizadores SET nome='" & txtNome.Text & "', " & "utilizador='" & txtUtilizador.Text & "', " & "senha='" & txtSenha.Text & "'," & "nivel=" & CbNivel.SelectedIndex & " WHERE ID=" & LblID.Text)
+        Catch ex As Exception
+            MessageBox.Show("ERRO" & ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            delete("DELETE FROM Utilizadores WHERE ID=" & LblID.Text)
+        Catch ex As Exception
+            MessageBox.Show("ERRO" & ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub DgLista_DoubleClick(sender As Object, e As EventArgs) Handles DGLista.DoubleClick
+        LblID.Text = DGLista.CurrentRow.Cells(0).Value
+        txtNome.Text = DGLista.CurrentRow.Cells(1).Value
+        txtUtilizador.Text = DGLista.CurrentRow.Cells(2).Value
+        txtSenha.Text = DGLista.CurrentRow.Cells(3).Value
+        CbNivel.SelectedIndex = DGLista.CurrentRow.Cells(4).Value
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtNome.Select()
+        Me.btnReload.PerformClick()
+    End Sub
+End Class
